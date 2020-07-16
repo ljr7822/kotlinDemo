64d6a1b699518abbe0b910e99499a380702bc4e1
iwenLjr <1816807688@qq.com>
2020-07-16 13:49:58 +0800

    java和kotlin的单例

--- src/com/iwen/kotlin/function/Util.java
+++ src/com/iwen/kotlin/function/Util.java
@@ -19,6 +19,47 @@ public class Util {
      * 实例方法 必须实例化类对象,用这个类的对象来创建
      */
     public void show(){
+        // 3.饿汉式访问单例
+        //Util1.instance
+    }
 
+}
+
+/**
+ * 饿汉式单例模式
+ */
+class Util1{
+    // 2.提供一个接收对象 外部可以访问
+    public static Util1 instance = new Util1();
+
+    // 1.私有化构造方法
+    private Util1(){
+        System.out.println("我是饿汉式单例模式，被访问到了");
+        // 4.访问懒汉式单例
+        Util2.getInstance();
+    }
+}
+
+/**
+ * 懒汉式单例模式
+ */
+class Util2{
+    // 2.提供一个接收对象 外部不可以访问
+    private static Util2 defatult;
+
+    // 1.私有化构造方法
+    private Util2(){
+        System.out.println("我是懒汉式单例模式，被访问到了");
+    }
+    // 3.提供一个外部访问的方法
+    public static Util2 getInstance(){
+        if (defatult == null){
+            synchronized (Util2.class){
+                if (defatult == null){
+                    defatult = new Util2();
+                }
+            }
+        }
+        return defatult;
     }
 }
---
+++ src/com/iwen/kotlin/function/singleton.kt
@@ -0,0 +1,51 @@
+package com.iwen.kotlin.function
+
+/**
+ * 单例
+ *
+ * @Author: iwen大大怪
+ * @DateTime: 2020/7/16 13:17
+ */
+fun main() {
+    // 3.饿汉式访问单例
+    Person.instance.show()
+    // 3.懒汉式访问单例
+    Sqlite.getInstance().show()
+}
+
+/**
+ * 饿汉式
+ */
+class Person private constructor(){
+    // 1.私有化构造方法 private constructor()
+    // 2.创建单例对象
+    companion object{
+        val instance = Person()
+    }
+    fun show(){
+        println("hello 饿汉式")
+    }
+}
+
+/**
+ * 懒汉式
+ */
+class Sqlite private constructor(){
+    // 1.私有化构造方法 private constructor()
+    // 2.创建单例对象
+    companion object{
+        var default:Sqlite? = null
+
+        fun getInstance():Sqlite{
+            if (default == null){
+                synchronized(this) {
+                    default = Sqlite()
+                }
+            }
+            return default!!
+        }
+    }
+    fun show(){
+        println("hello 懒汉式")
+    }
+}
